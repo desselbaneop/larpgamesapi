fdd6160cdd474db5f2d2d583d984d5f012b0c7bd
Dessel <desselbaneop@gmail.com>
2022-05-11 19:47:48 +0200

    some changes

--- src/main/java/ywa/interactive/larpgamesapi/models/entities/User.java
+++ src/main/java/ywa/interactive/larpgamesapi/models/entities/User.java
@@ -10,9 +10,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.util.Collection;
 import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Data
